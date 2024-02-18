1b17c0d3ecef64f65bbecd697ab51109a133f2b3
Betakyi <160411069+Betakyi@users.noreply.github.com>
2024-02-18 10:33:52 -0500

    Add files via upload
    updated

--- Op Trailler.py
+++ Op Trailler.py
@@ -34,10 +34,13 @@ sign.y = 0
 
 # Set up game variables
 score = 0
-
+lives = 3
+game_over = False
 
 def update():
     global score
+    global lives
+    global game_over
     ##### character ###
     # Move ship left and right#
     if keyboard.left:
@@ -73,17 +76,44 @@ def update():
         sign.y = 0
     #Sign collision with studnet
     if sign.colliderect(student):
-
+        sounds.lose.play()
         sign.x =random.randint(20,780)
         coin.y = 0
-        score = score -1
+        score -= 1
+        lives = lives -1
+
+    #End Games at 0 lives
+    if lives == 0:
+        game_over = True
+        coin.y = 0
+        sign.y = 0
+        dollars.y = 0
+
+
+    #Money###
+    #Bomb down the page
+    dollars.y += 5
+    #Reset at Top
+    if dollars.y > 600:
+        dollars.x = random.randint(20,780)
+        dollars.y = 0
+    # collision with ship
+    if dollars.colliderect(student):
+        sounds.explosion.play()
+        dollars.x = random.randint(20,780)
+        dollars.y = 0
 
 
 def draw():
     bg.draw()
-    student.draw()
+
+    if game_over:
+        screen.draw.text('GAME OVER', (230, 200), color = white, fontname = 'publicpixel', fontsize=30)
+        screen.draw.text('FINAL SCORE: ' + str(score), (180, 300), color = white, fontname = 'publicpixel', fontsize=30)
+    else:
+      student.draw()
     coin.draw()
     dollars.draw()
     sign.draw()
     screen.draw.text('Score: '+ str(score), (15, 10), color=(white), fontname = 'publicpixel', fontsize=15)
-
+    screen.draw.text('Lives: '+ str(lives), (650, 10), color=(white), fontname = 'publicpixel', fontsize=15)
